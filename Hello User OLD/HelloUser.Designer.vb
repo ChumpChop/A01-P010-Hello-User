b31b3a1760d456c0325c9f38994a66527b31068a
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-07 22:39:20 +0100

    Add project files.

---
+++ Hello User OLD/HelloUser.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HelloUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(12, 9)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(88, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Enter Your Name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(15, 25)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(12, 415)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 2
+        Me.btnOK.Text = "&OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(713, 415)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'HelloUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "HelloUser"
+        Me.Text = "Hello from Visual Basic 2010"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Hello User/HelloUser.vb
@@ -0,0 +1,16 @@
+﻿Public Class HelloUser
+
+    Private Sub btnOK_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnOK.Click
+        'Display a message box greeting to the user
+        MessageBox.Show("Hello, " & txtName.Text & _
+            "! Welcome to Visual Basic 2010.", _
+            "Hello User Message")
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnExit.Click
+        'End the program and close the form
+        Me.Close()
+    End Sub
+End Class
